875520aeb1299ffa510e6567c6cca664911ecc90
marianne <marianne.butler@gmail.com>
2022-10-28 19:18:39 +0100

    v2

--- site.js
+++ site.js
@@ -11,7 +11,7 @@ activities = mainContainer.querySelector('#activities');
 * cv html
 ***/
 const startYear = 1997, endYear = 2023, numYears = endYear - startYear,
-years = Array.from(Array(numYears).keys()),
+years = Array.from(Array(numYears).keys()), 
 monthPercentage = 100 / numYears / 12,
 appendSpiral = function({el, count = 1}) {
 	if (el == null) return;
@@ -21,7 +21,7 @@ appendSpiral = function({el, count = 1}) {
 		container.innerHTML = `
 			<svg viewBox="0 0 100 161.8">
 				<path d="M0,0 c55.3,0,100,44.7,100,100c0,34.2-27.6,61.8-61.8,61.8C17.1,161.8,0,144.7,0,123.6c0-13,10.6-23.6,23.6-23.6c8.1,0,14.6,6.5,14.6,14.6c0,5-4,9-9,9c-3.1,0-5.6-2.5-5.6-5.6c0-1.9,1.5-3.4,3.4-3.4"></path>
-				<line x1="0" x2="-161.8" y1="0" y2="0"></line>
+				<line x1="-161.8" x2="0" y1="0" y2="0"></line>
 			</svg>`;
 		el.appendChild(container);
 	}
@@ -34,7 +34,6 @@ calculatePosition = function({toYear, toMonth, fromYear = null, fromMonth = 1}) 
 };
 
 async function setupHtml() {
- 	timeline.setAttribute("data-years", numYears);
 	timeline.innerHTML = years.reverse().reduce(function(str, year) {
 		return str + `<li data-year="${startYear + year + 1}"></li>`;
 	}, "");
@@ -48,26 +47,12 @@ async function setupHtml() {
 		});
 		act.style.top = `${activityTop * monthPercentage}%`;
 		act.style.minHeight = `${activityLength * monthPercentage}%`;
+		const foot = document.createElement("footer");
+		foot.innerHTML = `<small>${act.querySelector("h5").innerHTML}</small>`;
+		act.appendChild(foot);
+		appendSpiral({el: foot, count: 2});
 		appendSpiral({el: act.querySelector("h4")});
 		appendSpiral({el: act.querySelector("h5")});
-
-		if (activityLength > 3) {
-			const foot = document.createElement("footer");
-			foot.innerHTML = `<small>
-				${act.getAttribute("data-start-year")}: 
-				<strong>${act.querySelector("strong").innerHTML}</strong>
-			</small>`;
-			appendSpiral({el: foot, count: 2});
-			act.appendChild(foot);
-
-			act.querySelectorAll("li").forEach(function(eve) {
-				const [eventPosition, eventlength] = calculatePosition({
-					toYear: eve.getAttribute("data-event-year"),
-					toMonth: eve.getAttribute("data-event-month")
-				});
-				eve.style.top = `${(eventPosition - activityTop) / activityLength * 100}%`;
-			});
-		} // else it's a one-off project
 	});
   	return;
 };
@@ -83,12 +68,11 @@ async function initCV() {
 			items.forEach(function({target, isIntersecting}) {
 				target.setAttribute("data-state", isIntersecting ? "in" : "out")
 				target.nextElementSibling.setAttribute("data-header-state", isIntersecting ? "in" : "out");
-				if (isIntersecting) target.nextElementSibling.setAttribute("data-scroll-direction", "down");
 			});
 		}, 
 		{
 			root: mainContainer,
-	  		threshold: [0.9]
+	  		threshold: [0]
 		}
 	);
 
@@ -99,16 +83,29 @@ async function initCV() {
 			items.forEach(function({target, isIntersecting}) {
 				target.setAttribute("data-state", isIntersecting ? "in" : "out")
 				target.previousElementSibling.setAttribute("data-footer-state", isIntersecting ? "in" : "out");	
-				if (isIntersecting) target.previousElementSibling.setAttribute("data-scroll-direction", "up");
 			});
 		}, 
 		{
 			root: mainContainer,
-	  		threshold: [0.9]
+	  		threshold: [0]
 		}
 	);
 
 	activities.querySelectorAll('footer').forEach((el) => ioFooter.observe(el));
+
+	const ioContent = new IntersectionObserver(
+		function(items) {
+			items.forEach(function({target, isIntersecting}) {
+				target.setAttribute("data-state", isIntersecting ? "in" : "out")
+			});
+		}, 
+		{
+			root: mainContainer,
+	  		threshold: [0]
+		}
+	);
+
+	activities.querySelectorAll('figure').forEach((el) => ioContent.observe(el));
 }
 
 initCV();
@@ -120,19 +117,19 @@ const setSplash = (isSplash) => document.body.setAttribute("data-state", isSplas
 scrollMain = ([top, left]) => mainContainer.scrollTo({top: top, left: left, /*behavior: 'smooth'*/});
 
 header.querySelector("[data-action=scroll]").addEventListener("click", () => setSplash(false));
-header.querySelector("[data-action=splash]").addEventListener("click", () => scrollMain([0,0]));
+header.querySelector("[data-action=splash]").addEventListener("click", function() {
+	setSplash(true);
+	scrollMain([0,0]);
+});
 
 hamburger.addEventListener("toggle", function(event) {
 	const showContent = (document.body.getAttribute("data-state") === "splash");
 	if (hamburger.getAttribute("open") == null) {
-		console.log("hamburger toggle closed")
 		if (showContent) hamburger.setAttribute("open", ""); // force it to open
 		else hamburger.querySelector("summary").blur(); // remove the focus
 	}
-	else console.log("hamburger toggle opened")
 
 	if (showContent) {
-		console.log("force skills open and scroll")
 		hamburger.querySelector("details").setAttribute("open", ""); // ensure the first child is open
 		setSplash(false); // do this last
 	}
